44a1e2ca0e210c9f3cd2aefad2756aafb4b3d7f6
EndreUjvari94 <info@endreujvari.com>
2023-02-28 17:46:24 +0100

    Making showroom selection more interactive

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { ImageService } from '../services/image.service';
 import { CUT_TYPE, BLURRING_LICENSE_PLATE } from '../globals';
 import { UserService } from '../services/user.service';
+import { Showroom } from '../models';
 
 @Component({
   selector: 'app-settings',
@@ -13,6 +14,8 @@ export class SettingsComponent {
   @Output() changeView = new EventEmitter<string>();
 
   isShowroomSelectionActive = false;
+  showroomImage: Showroom = this.imageService.showrooms[0];
+
   
   constructor(
     public imageService: ImageService,
@@ -21,6 +24,7 @@ export class SettingsComponent {
 
   onShowroomSelect(event: any) {
     this.userService.sceneId = event.target.name;
+    this.toggleShowroomSelection();
   }
 
   toggleShowroomSelection() {
@@ -33,6 +37,10 @@ export class SettingsComponent {
 
   getBlurringLicensePlateModel() {
     return BLURRING_LICENSE_PLATE;
+  }
+
+  setShowroomImage(event: any) {
+    this.showroomImage = <Showroom>this.imageService.showrooms.find(x => x.name === event.target.name);
   }
 
   onGenerate() {
